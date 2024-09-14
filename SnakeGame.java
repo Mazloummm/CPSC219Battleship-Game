17dbda0d9cc025adb574279a06d90f47f4b5bf05
atsion <156040084+atsion@users.noreply.github.com>
2024-09-14 01:43:26 +0000

    fixed variable names

--- SnakeGame.java
+++ SnakeGame.java
@@ -3,13 +3,13 @@ import java.util.Random;
 
 public class SnakeGame {
     // Constants declared
-public static final int Grid_Size_X = 20;    // Width of row
-public static final int Grid_Size_Y = 10;  // Height of column
-public static final int Max_Snake_Length = 100;
-public static final char Snake_Char = 'S'; // Snake on the grid
-public static final char Food_Char = '@'; // FOod on the grid
-public static final char Empty_Char = '.'; // Empty space of the grid
-public static final char Wall_Char = '#'; // Wall of the grid
+public static final int GRID_SIZE_X = 20;    // Width of row
+public static final int GRID_SIZE_Y = 10;  // Height of column
+public static final int MAX_SNAKE_LENGTH = 100;
+public static final char SNAKE_CHAR = 'S'; // Snake on the grid
+public static final char FOOD_CHAR = '@'; // FOod on the grid
+public static final char EMPTY_CHAR = '.'; // Empty space of the grid
+public static final char WALL_CHAR = '#'; // Wall of the grid
 
 
 // Variables declared
@@ -20,30 +20,30 @@ public static int snake[][]; // Body of snake
 // Initialize the grid
 
 public static void initializeGame() {
-    grid = new char[Grid_Size_Y][Grid_Size_X];
+    grid = new char[GRID_SIZE_Y][GRID_SIZE_X];
     Random random = new Random();
     // Loop for creating walls, borders, and empty space.
-    for (int y = 0; y < Grid_Size_Y; y++) {
-        for (int x = 0; x < Grid_Size_X; x++) {
-            if (y == 0 || y == Grid_Size_Y-1 || x == 0 || x == Grid_Size_X-1) {
-                grid[y][x] = Wall_Char;  // create walls             
+    for (int y = 0; y < GRID_SIZE_Y; y++) {
+        for (int x = 0; x < GRID_SIZE_X; x++) {
+            if (y == 0 || y == GRID_SIZE_Y-1 || x == 0 || x == GRID_SIZE_X-1) {
+                grid[y][x] = WALL_CHAR;  // create walls             
             } else {
-                grid[y][x] = Empty_Char; // create empty space
+                grid[y][x] = EMPTY_CHAR; // create empty space
             }
         }
     }
     generateFood(); // Generation of Food
  // Initialize the snake
 int snakeLength = 3;
-snake = new int[Max_Snake_Length][2]; // Space for snake
+snake = new int[MAX_SNAKE_LENGTH][2]; // Space for snake
 int startX = 0, startY = 0;
 
 // Generate a valid random coordinate start
 do {
     // Randomized position of snake within valid grid boundaries (not walls)
-    startX = random.nextInt(Grid_Size_X - 2) + 1; // Random value between 1 and Grid_Size_X-2
-    startY = random.nextInt(Grid_Size_Y - 2) + 1; // Random value between 1 and Grid_Size_Y-2
-} while (grid[startY][startX] != Empty_Char); // Loop runs until an empty space is found
+    startX = random.nextInt(GRID_SIZE_X - 2) + 1; // Random value between 1 and GRID_SIZE_X-2
+    startY = random.nextInt(GRID_SIZE_Y - 2) + 1; // Random value between 1 and GRID_SIZE_Y-2
+} while (grid[startY][startX] != EMPTY_CHAR); // Loop runs until an empty space is found
 
 
 // Place the snake on the grid
@@ -51,7 +51,7 @@ for (int i = 0; i < snakeLength; i++) {
     snake[i][0] = startY + i;  // Y-coordinate of the snake segment
     snake[i][1] = startX;      // X-coordinate of the snake segment
     if (i==0) {
-        grid[snake[i][0]][snake[i][1]] = Snake_Char; // Add the snake head
+        grid[snake[i][0]][snake[i][1]] = SNAKE_CHAR; // Add the snake head
 
     }
 }
@@ -69,8 +69,8 @@ public static void generateFood() {
     }
 
 public static void displayGrid() {
-    for (int y = 0; y < Grid_Size_Y; y++) {
-      for (int x = 0; x < Grid_Size_X; x++) {
+    for (int y = 0; y < GRID_SIZE_Y; y++) {
+      for (int x = 0; x < GRID_SIZE_X; x++) {
         System.out.print(grid[y][x]); // Print each cell without newline
       }
       System.out.println(); // Newline after each row
